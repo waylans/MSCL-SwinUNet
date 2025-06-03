e7a32075a591010ae8ee3f8f5cf843c54078823c
wnorth <136388058@qq.com>
2025-06-04 05:44:38 +0800

    Update __init__.py

--- mmseg/models/__init__.py
+++ mmseg/models/__init__.py
@@ -7,10 +7,9 @@ from .losses import *  # noqa: F401,F403
 from .necks import *  # noqa: F401,F403
 from .segmentors import *  # noqa: F401,F403
 
-## added by LYU: 2022/03/28
-from .discriminators import * # noqa: F401,F403
+
 
 __all__ = [
     'BACKBONES', 'HEADS', 'LOSSES', 'SEGMENTORS', 'build_backbone',
-    'build_head', 'build_loss', 'build_segmentor', 'build_discriminator'
+    'build_head', 'build_loss', 'build_segmentor'
 ]
