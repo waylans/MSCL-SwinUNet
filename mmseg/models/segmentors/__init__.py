9859cfba06a8d32ba5249cdd7b87830b3f3d3687
wnorth <136388058@qq.com>
2025-06-04 03:08:19 +0800

    Update __init__.py

--- mmseg/models/segmentors/__init__.py
+++ mmseg/models/segmentors/__init__.py
@@ -4,13 +4,5 @@ from .cascade_encoder_decoder import CascadeEncoderDecoder
 from .encoder_decoder import EncoderDecoder
 from .encoder_decoder_forAdap import EncoderDecoder_forAdap
 
-## added by LYU: 2022/04/22
-from .encoder_decoder_forDS2Net import EncoderDecoder_forDS2Net
-## added by LYU: 2022/05/17
-from .encoder_decoder_forEGAdap import EncoderDecoder_forEGAdap
-## added by LYU: 2023/07/20
-from .encoder_decoder_forDAFormer import EncoderDecoder_forDAFormer
-## added by LYU: 2023/07/21
-from .encoder_decoder_forDDFSeg import EncoderDecoder_forDDFSeg
 
-__all__ = ['BaseSegmentor', 'EncoderDecoder', 'CascadeEncoderDecoder', 'EncoderDecoder_forAdap', 'EncoderDecoder_forDS2Net', 'EncoderDecoder_forEGAdap', 'EncoderDecoder_forDAFormer', 'EncoderDecoder_forDDFSeg']
+__all__ = ['BaseSegmentor', 'EncoderDecoder', 'CascadeEncoderDecoder', 'EncoderDecoder_forAdap']
