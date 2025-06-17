4d96a5d6ecc1af6414fde4ca58df7f623de23b56
waylans <136366058@qq.com>
2025-06-17 15:47:42 +0800

    del

--- configs/README.md
+++
@@ -1,1 +0,0 @@
-### MMSegmentation configurations
--- mmseg/core/runners/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .dynamic_iterbased_runner import DynamicIterBasedRunner
-
-__all__ = ['DynamicIterBasedRunner']
